3c1ee9d452bd7d788e0bd738b773d0b7fa0be6ff
Marika Allely and Veronica Alley <veronica.aubry.alley@gmail.com>
2016-01-19 16:27:23 -0800

    added approximate functionality

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,35 @@ $(document).ready(function() {
     var string = ($("input#string").val());
     var squishString = string.replace(/\s+/g, '');
     var stringLength = squishString.length;
-    var result = square(stringLength);
-  console.log(squishString);
-  console.log(stringLength);
-  console.log(result);
+
+
+    var chunks = [];
+    var n = Math.sqrt(stringLength);
+    for (var i = 0; i < stringLength; i += n) {
+    chunks.push(squishString.substring(i, i + n));
+    }
+
+    var chunksArrange = [];
+
+    var letterSwap = function (letter) {
+    for (var i = 0; i < letter.length; i += 1) {
+        chunksArrange.push((letter[i].charAt(0)));
+      }
+    }
+
+    letterSwap(chunks);
+
+    console.log(chunksArrange);
+    var letters = chunksArrange.join("");
+      console.log(letters);
+
+
+
+
+    $(".not").text(chunks);
+    $("#result").show(chunks);
+    console.log(stringLength);
+
   event.preventDefault();
 
   });
